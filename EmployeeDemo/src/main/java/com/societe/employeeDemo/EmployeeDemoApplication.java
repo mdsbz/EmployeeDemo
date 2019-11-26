1ad154cce13b0c90c5367b1019a83a5383ae9dfd
Md Sbz <Md Sbz@192.168.1.122>
2019-11-27 03:55:19 +0530

    Application

---
+++ EmployeeDemo/src/main/java/com/societe/employeeDemo/EmployeeDemoApplication.java
@@ -0,0 +1,17 @@
+package com.societe.employeeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Md Sbz
+ *
+ */
+@SpringBootApplication
+public class EmployeeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDemoApplication.class, args);
+	}
+
+}
