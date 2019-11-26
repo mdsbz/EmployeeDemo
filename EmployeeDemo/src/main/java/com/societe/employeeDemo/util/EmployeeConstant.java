3ef44b5f86844bef6aa3a873a291d1626f65a2b6
Md Sbz <Md Sbz@192.168.1.122>
2019-11-27 04:00:23 +0530

    Project Constant

---
+++ EmployeeDemo/src/main/java/com/societe/employeeDemo/util/EmployeeConstant.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.societe.employeeDemo.util;
+
+/**
+ * @author Md Sbz
+ *
+ */
+public class EmployeeConstant {
+
+	public static final String SUCCESS = "SUCCESS";
+
+	public static final String FAILURE = "FAILURE";
+
+	public static final String NO_DATA_FOUND = "No Data Found";
+
+	public static final String NO_RECORD_FOUND = "No Record Found";
+
+}
